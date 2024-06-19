3dad0519161077deb79292f81d9c38423891ebfb
miklegonza <gonzalez.michael1803@gmail.com>
2024-06-18 20:55:43 -0500

    Cambios pequeños

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,10 +9,17 @@ const {
 
 const router = Router();
 
+/* Usuarios */
 router.get('/users', getUsers);
 router.get('/users/:username', getUserByUsername);
 router.post('/users', createUser);
 router.put('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
+
+/* Componentes */
+
+
+/* Pedidos */
+
 
 module.exports = router;
